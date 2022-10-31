7a079397d97dfba2b2da58bebaac24401991cf71
Osunronbi Okikiola <osunronbiolanlesi@gmail.com>
2022-11-01 00:35:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    slackUsername: "Okikiola",
+    backend: true,
+    age: 21,
+    bio: "I am a backend developer",
+  });
+});
+
+app.listen(3100, "localhost", (err, data) => {
+  if (err) {
+    console.log("Error occured");
+  } else {
+    console.log("Server is running on port 3100");
+  }
+});
